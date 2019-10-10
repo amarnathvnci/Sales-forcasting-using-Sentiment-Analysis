eca93ee0da0871adda8f2a021635e6acad74c6ee
Amarnath Venkataramanan <amarnathfire@gmail.com>
2019-10-10 14:04:12 +0100

    Sales Forecasting

---
+++ wine.R
@@ -0,0 +1,296 @@
+# Installing packages
+install.packages("tidyverse")
+install.packages("janitor")
+install.packages("purr")
+install.packages("psych")
+install.packages("tidytext")
+install.packages("randomcoloR")
+install.packages("broom")
+install.packages("ggrepel")
+install.packages("caret")
+install.packages("e1071")
+install.packages("randomForest")
+install.packages("RColorBrewer")
+install.packages("sentimentr")
+install.packages("dplyr")
+# Loading Libraries -------------------------------------------------------
+library(tidyverse)
+library(janitor)
+library(purrr)
+library(psych)
+library(tidytext)
+library(randomcoloR)
+library(tidytext)
+library(broom)
+library(ggrepel)
+library(caret)
+library(e1071)
+library(randomForest)
+library(RColorBrewer)
+library(sentimentr)
+library(dplyr)
+sentiments
+
+#get_applements("afinn")
+
+# Reading File -----------------------------------------------------------
+apple <- read.csv("C:/Users/admin/Desktop/Research Project/apple.csv") %>% clean_names()
+
+
+# Exploring Data ----------------------------------------------------------
+
+glimpse(apple)
+str(apple)
+
+# Checking number of na
+map_df(apple, ~ sum(is.na(.x)))
+
+# Only 13695 NA values so omitting that
+apple <- apple %>% na.omit()
+
+# Checking distribution + correlation using pairs.panels
+pairs.panels(apple[1:100, 2:5])
+pairs.panels(apple[1:100, 6:11])
+
+
+# Exploring & Visualising Data --------------------------------------------
+
+# Distriution of Points Variable
+
+ggplot(data = apple, aes(x = points, colour = I("black"), fill = I("#099DD9"))) +
+  geom_histogram(binwidth = 1) +
+  labs(x = "Points", y = "Frequency", title = "Distribution of points")
+#--------------------------
+
+# Distribution of Price variable
+ggplot(data = apple, aes(x = price, colour = I("black"), fill = I("#099DD9"))) +
+  geom_histogram() +
+  labs(x = "Price", y = "Frequency", title = "Distribution of prices")
+
+# Strongly right skewed, using log to plot apple prices
+#--------------------------
+
+ggplot(data = apple, aes(x = log(price), colour = I("black"), fill = I("#099DD9"))) +
+  geom_histogram() +
+  labs(x = "log(Price)", y = "Frequency", title = "Distribution of log(prices)")
+
+# slightly right skewed
+#--------------------------
+
+
+# Checking the word count from reviews for each apple
+apple$wordcount <- sapply(gregexpr("\\S+", apple$text), length)
+
+ggplot(data = apple, aes(x = wordcount, colour = I("black"), fill = I("#099DD9"))) +
+  geom_histogram(binwidth = 3) +
+  labs(x = "Word Count", y = "Frequency", title = "Distribution of word count of text")
+
+# slightly right skewed
+#--------------------------
+
+# Country with most costly apples
+
+apple %>%
+  group_by(country) %>%
+  summarise(avg_price = mean(price)) %>%
+  top_n(10) %>%
+  ggplot(aes(x = reorder(country, -avg_price), avg_price, fill = country)) + geom_bar(stat = "identity") + labs(title = "Country with Highest apple Prices", x = "", y = "apple Price") + scale_fill_brewer(palette = "Paired") + theme_bw() + theme(legend.position = "none", plot.title = element_text(hjust = 0.5), axis.text.x = element_text(angle = 45, hjust = 1))
+#--------------------------
+
+# Score Vs Price
+
+ggplot(apple, aes(x = price, y = points)) +
+  geom_jitter(shape = 1) + geom_smooth() +
+coord_cartesian(xlim = c(0, 4000), ylim = c(75, 100)) +
+  labs(title = "Score vs Price", x = "Price", y = "Score")
+
+ggplot(apple, aes(x = price, y = points)) +
+  geom_jitter(shape = 1) +
+  coord_cartesian(xlim = c(0, 40), ylim = c(75, 100)) +
+  labs(title = "Score vs Price", x = "Price", y = "Score")
+
+# It appears that the correlation between apple price and points given levels off around $40! Additinally, it is clear from the first larger plot that spending thousands of dollars on a bottle of apple is usually a poor decision and a comparable bottle can be had for much less
+
+#--------------------------
+
+# Explotation of Most Common Words
+
+apple$text <- as.character(apple$text)
+apple$sentiment <- as.numeric(apple$sentiment)
+
+apple = apple %>%
+  mutate(grade = ifelse(sentiment > "3","Good",ifelse(sentiment > "2","Average","Bad")))
+#Creating Grade column based on points
+
+apple$grade = as.factor(apple$grade)
+apple$grade = factor(apple$grade,levels(apple$grade)[c(3,1,2)])
+
+#Finding most common words
+apple_Words = apple %>% dplyr::select(text, grade)%>%
+  unnest_tokens(word, text)%>%
+  anti_join(stop_words)%>%
+  dplyr::filter(!str_detect(word, "[0-9]"))
+
+colourCount = 15
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+
+
+apple_Words %>%
+  count(word, sort = TRUE) %>%
+  top_n(15) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n, fill = word)) +
+  geom_col() +
+  labs(title = "Top 15 Significant Words From All Comments", x = "Total Number of Occurances", y = "Word") +
+  coord_flip()+  scale_fill_manual(values = getPalette(colourCount))+
+  theme_bw() + theme(legend.position = "none", plot.title = element_text(hjust = 0.5))
+
+#--------------------------
+
+comment_words = apple_Words %>%
+  count(grade, word, sort = TRUE) %>%
+  ungroup()
+
+total_words = comment_words %>% 
+  group_by(grade) %>% 
+  summarize(total = sum(n))
+
+grade_words = left_join(comment_words, total_words)
+
+grade_words = grade_words %>%
+  bind_tf_idf(word, grade, n)
+
+
+#Words from Good review
+grade_words %>% 
+  arrange(desc(tf_idf)) %>%
+  mutate(word = factor(word, levels = rev(unique(word)))) %>% 
+  group_by(grade) %>% 
+  top_n(10) %>% 
+  ungroup %>%
+  dplyr::filter(grade == "Good")%>%
+  ggplot(aes(word, tf_idf)) +
+  geom_col(show.legend = FALSE, fill = 'red') +
+  labs(title ="Top 10 Significant Words from Good posts", x = NULL, y = "tf-idf") +
+  coord_flip()+theme_bw()
+
+#Words from Bad review
+grade_words %>% 
+  arrange(desc(tf_idf)) %>%
+  mutate(word = factor(word, levels = rev(unique(word)))) %>% 
+  group_by(grade) %>% 
+  top_n(10) %>% 
+  ungroup %>%
+  dplyr::filter(grade == "Bad")%>%
+  ggplot(aes(word, tf_idf)) +
+  geom_col(show.legend = FALSE, fill = 'Blue') +
+  labs(title ="Top 10 Significant Words from Bad reviews", x = NULL, y = "tf-idf") +
+  coord_flip()
+
+
+# Feature Extraction ------------------------------------------------------
+
+
+#sentiment analysis using sentiment() function
+score <- sentiment(apple$text, polarity_dt = lexicon::hash_sentiment_jockers_rinker, valence_shifters_dt = lexicon::hash_valence_shifters, hyphen = "")
+score1 <- aggregate(sentiment~element_id, data=score, FUN=sum) 
+apple$score <- score1$sentiment
+sum(apple$score) # obtained sum of sentiment score as -19.99 / 3866 = -0.0052, so drop in the rating value
+
+#Use this in program as well https://datascienceplus.com/automated-text-feature-engineering-using-textfeatures-in-r/
+## This link shows that there is drop in share price of apple during december 2014 https://www.macrotrends.net/stocks/charts/AAPL/apple/stock-price-history
+##AS our total sentiment score is negative it is proven that lower sentment score drops the value of company 
+
+#Sentiment Analysis 
+
+word_final <- inner_join(apple_Words, grade_words[,c("grade","word","tf")], by=c("grade","word"))
+
+word_final <- inner_join(word_final, get_sentiments("afinn"))
+
+sentiment_score <- word_final %>% group_by(grade) %>% summarise(sentiment_score=mean(value)) 
+
+apple_final <- full_join(apple, sentiment_score)
+
+apple_final %>% group_by(grade) %>% summarise(mean(sentiment_score,na.rm = TRUE))
+
+temp <- apple_final %>% dplyr::filter(is.na(sentiment_score)) %>% 
+  mutate(sentiment_score= ifelse(grade=="Good",1.76,ifelse(grade=="Average",1.54,1.13)))
+
+apple_final <- apple_final %>% na.omit()
+
+apple <- unique(rbind(apple_final,temp))
+
+head(apple)
+
+
+#change sentiment score
+score1 <- sapply(score, FUN = function(x) {ifelse(x==1.74,1.74,ifelse(x==1.54,0,1.13))})
+#Volume of missing value
+apple$sentiment_score
+
+
+#We have created three extra columns in our apple dataset, one for word count, second for grade (based on points) & third score (basis applement score from description of apple)
+
+
+# Model Building ----------------------------------------------------------
+#Creating Test & Train
+
+apple <- apple %>% dplyr::select(points,price,applement_score,wordcount,country)
+dmy <- dummyVars(" ~ .", data = apple,fullRank = T)
+apple_transformed <- data.frame(predict(dmy, newdata = apple))
+
+splitIndex <- createDataPartition (apple_transformed$points, p=.70, list=FALSE, times=1)
+
+train <- apple_transformed[splitIndex, ]
+test <- apple_transformed[-splitIndex, ]
+
+
+
+# BUILD MODELS ------------------------------------------------------------
+
+#KNN MODEL
+
+RMSE = function(m, o){
+  sqrt(mean((m - o)^2))
+}
+
+
+#trctrl <- trainControl(method = "repeatedcv", number = 5)
+#set.seed(3333)
+knn_fit <- train(points ~., data = train[1:2000,], method = "knn")
+
+test$pred_knn <- predict(knn_fit, newdata = test)
+
+
+#SVM
+
+trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
+
+svm_Linear <- train(points ~., data = train[1:2000,], method = "svmLinear",trControl=trctrl)
+                    
+                  
+test$pred_svm <- predict(svm_Linear, newdata = test)   
+                    
+#LINEAR REGRESSION
+                    
+linear_model <- lm(points ~., data = train[1:2000,])
+test$pred_lm <- predict(linear_model, newdata = test)   
+                    
+             
+                    
+                    
+#RmSE on Test from all algorithm
+
+print("RMSE FOR KNN model is ");RMSE(test$pred_knn,test$points)
+print("RMSE FOR SVM model is ");RMSE(test$pred_svm,test$points)
+print("RMSE FOR LM model is ");RMSE(test$pred_lm,test$points)
+
+result1 <- data.frame("Actual value" = test$points, "Predicted value" = test$pred_lm)
+  result1
+plot(result1, type= "o")
+
+
+
+
+sentiment_by("My life has become terrible since I met you and lost money. But I still have got a little hope left in me") %>%
+  highlight()
